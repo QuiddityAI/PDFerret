071c8af6d251c323c16732778fa7bbfff84d7d6a
Andrii Trelin <andrei.trel2010@gmail.com>
2024-04-14 12:24:00 +0200

    Added logging

---
+++ src/pdfferret/logging.py
@@ -0,0 +1,12 @@
+import logging
+import logging.handlers
+logger = logging.getLogger()
+logger.setLevel(logging.WARN)
+handler = logging.handlers.RotatingFileHandler(
+    "pdferret.log", maxBytes=(1048576*5), backupCount=10
+)
+formatter = logging.Formatter(
+    "%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+logger.addHandler(logging.StreamHandler())
