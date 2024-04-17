800e1cf2e3f18880f941e3f95bee8f263b8b9538
Andrii Trelin <andrei.trel2010@gmail.com>
2024-04-17 11:47:33 +0200

    Added missing config.py

---
+++ src/pdferret/config.py
@@ -0,0 +1,19 @@
+import multiprocessing
+import os
+from .logging import logger
+
+NPROC = multiprocessing.cpu_count()
+if nproc_env := os.environ.get("PDFERRET_NPROC"):
+    NPROC = int(nproc_env.strip())
+
+
+BATCH_SIZE = 2*NPROC
+if bsize_env := os.environ.get("PDFERRET_BATCH_SIZE"):
+    BATCH_SIZE = int(bsize_env.strip())
+
+logger.info(
+    f"Using {NPROC} CPUs and {BATCH_SIZE} batch size")
+
+GROBID_URL = "http://localhost:8070"
+if url_env := os.environ.get("PDFERRET_GROBID_URL"):
+    GROBID_URL = url_env
